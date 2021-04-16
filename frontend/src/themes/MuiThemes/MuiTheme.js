62ad064e2ef81c091c22453fd2c11e3b5c3d9a83
Lucas Felipe <conhecimento54321@gmail.com>
2021-04-16 00:35:29 -0300

    Create Matherial-UI theme

---
+++ frontend/src/themes/MuiThemes/MuiTheme.js
@@ -0,0 +1,41 @@
+import { createMuiTheme, responsiveFontSizes } from "@material-ui/core";
+
+let MuiTheme = createMuiTheme({
+  overrides: {
+    MuiAppBar: {
+      root: {
+        boxShadow: 'none',
+      }
+    }
+  },
+
+  palette: {
+    primary: {
+      main: '#001a35f2'
+    },
+    secondary: {
+      main: '#2ee897e7',
+      contrastText: '#fff'
+    },
+    background: {
+      default: '#ebebeb'
+    }
+  },
+
+  typography: {
+    button: {
+      letterSpacing: 3
+    }
+  },
+
+  breakpoints: {
+    values: {
+      sm: 370,
+      md: 600
+    },
+  }
+})
+
+MuiTheme = responsiveFontSizes(MuiTheme)
+
+export default MuiTheme
