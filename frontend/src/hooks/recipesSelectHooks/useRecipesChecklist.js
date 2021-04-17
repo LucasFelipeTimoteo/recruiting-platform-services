baa424cc185e413c1d02cb5a8a1d41156021734e
Lucas Felipe <conhecimento54321@gmail.com>
2021-04-17 02:47:27 -0300

    Create useRecipesChecklist hook

---
+++ frontend/src/hooks/recipesSelectHooks/useRecipesChecklist.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react'
+import toggleRecipeCheckbox from '../../utils/RecipeSelect/toggleRecipeCheckbox'
+
+export default function useRecipesChecklist(recipes) {
+  const [recipesChecklist, setRecipesCheckList] = useState([])
+
+  useEffect(() => {
+    const checklist = recipes?.map(recipe => (
+      { ...recipe, checked: false }
+    ))
+
+    setRecipesCheckList(checklist)
+  }, [recipes])
+
+  const handleRecipeChecklist = (selectedRecipe) => {
+    const checkedRecipes = recipesChecklist.filter(recipe => recipe.checked)
+    const allowMoreChecks = checkedRecipes.length < 2
+
+    const newRecipesChecklist = toggleRecipeCheckbox(
+      recipesChecklist,
+      selectedRecipe,
+      allowMoreChecks
+    )
+
+    setRecipesCheckList(newRecipesChecklist)
+  }
+
+  return {
+    recipesChecklist,
+    handleRecipeChecklist
+  }
+}
