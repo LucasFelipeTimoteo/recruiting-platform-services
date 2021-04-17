22636178bc3de29f001be4ddb976a712dcbbe0b2
Lucas Felipe <conhecimento54321@gmail.com>
2021-04-17 13:21:02 -0300

    Implement alreadyChecked util function test

---
+++ frontend/src/utils/RecipeSelect/toggleRecipeCheckbox/alreadyChecked/__tests__/units/alreadyChecked.test.js
@@ -0,0 +1,8 @@
+describe('only return true is checked property is true, else, return false', () => {
+  test('should return true', () => {
+    const expected = true
+    const checked = true
+
+    expect(expected).toBe(checked)
+  })
+})
