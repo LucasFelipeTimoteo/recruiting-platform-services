41050fe90db4556f7c3cde5a76d3f3dc279f9d1d
Lucas Felipe <conhecimento54321@gmail.com>
2021-04-17 02:40:38 -0300

    Create and style recipeTime component

---
+++ frontend/src/parts/RecipesSelectParts/RecipeTime/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Typography } from '@material-ui/core'
+import { History as TimeIcon } from '@material-ui/icons'
+
+import useTimeStyles from './styles'
+import parseNumberToMinutes from '../../../utils/RecipeSelect/parseNumberToMinutes'
+
+export default function RecipeTime({ recipe }) {
+  const styles = useTimeStyles()
+  const parsedTime = parseNumberToMinutes(recipe.time)
+
+  return (
+    <div className={styles.timeWrapper}>
+      <TimeIcon className={styles.timeIcon} />
+      <Typography variant="subtitle2" className={styles.time}>
+        {parsedTime}
+      </Typography>
+    </div>
+  )
+}
---
+++ frontend/src/parts/RecipesSelectParts/RecipeTime/styles.js
@@ -0,0 +1,15 @@
+import { makeStyles } from "@material-ui/core";
+
+const useRecipeTimeStyles = makeStyles({
+  timeWrapper: {
+    display: 'flex',
+    alignItems: 'center'
+  },
+
+  timeIcon: {
+    width: 15,
+    marginRight: 2
+  }
+})
+
+export default useRecipeTimeStyles
