bbbf963ddac7a566a6ac0149362c45f3ff045dc0
Lucas Felipe <conhecimento54321@gmail.com>
2021-04-17 02:46:41 -0300

    Create page Recipes

---
+++ frontend/src/pages/Recipes/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Header from '../../components/Header'
+import RecipesSelect from '../../components/RecipesSelect'
+
+export default function Recipes() {
+  return (
+    <>
+      <Header />
+      <RecipesSelect />
+    </>
+  )
+}
