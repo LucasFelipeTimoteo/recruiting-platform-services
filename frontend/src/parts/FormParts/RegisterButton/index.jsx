372c311dc4a5f6fab448f8b2bfbc937f5545920b
Lucas Felipe <conhecimento54321@gmail.com>
2021-04-20 23:39:50 -0300

    Remove Link component from RegisterButton part

--- frontend/src/parts/FormParts/RegisterButton/index.jsx
+++ frontend/src/parts/FormParts/RegisterButton/index.jsx
@@ -1,26 +1,22 @@
 import React from 'react'
 import { Button } from '@material-ui/core'
-
 import useRegisterButton from './styles'
-import { Link } from 'react-router-dom'
-
 
 export default function RegisterButton() {
   const styles = useRegisterButton()
 
   return (
     <>
-      <Link to="recipes" className={styles.buttonLink}>
-        <Button
-          color="primary"
-          variant="contained"
-          size="large"
-          disableElevation={true}
-          className={styles.button}
-        >
-          cadastrar
+      <Button
+        type="button"
+        color="primary"
+        variant="contained"
+        size="large"
+        disableElevation={true}
+        className={styles.button}
+      >
+        cadastrar
       </Button>
-      </Link>
     </>
   )
 }
