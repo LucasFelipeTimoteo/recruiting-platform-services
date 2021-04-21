db02c87ef8f1b5f7555aa94f17d3f4807f5da46d
Lucas Felipe <conhecimento54321@gmail.com>
2021-04-20 23:06:20 -0300

    Add setRecipesStorage to ListItem part

--- frontend/src/parts/RecipesSelectParts/ListItem/index.jsx
+++ frontend/src/parts/RecipesSelectParts/ListItem/index.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
+import setRecipesStorage from '../../../utils/RecipesSelect/setRecipesStorage'
 import useListItemStyles from './styles'
 
-export default function ListItem({ children, recipe, handleRecipeChecklist }) {
+export default function ListItem({
+  children,
+  recipe,
+  recipesChecklist,
+  handleRecipeChecklist,
+}) {
   const styles = useListItemStyles()
 
   return (
     <>
       <li
         className={styles.listItem}
-        onClick={() => handleRecipeChecklist(recipe)}
+        onClick={() => {
+          handleRecipeChecklist(recipe)
+          setRecipesStorage(recipesChecklist)
+        }}
       >
         {children}
       </li>
