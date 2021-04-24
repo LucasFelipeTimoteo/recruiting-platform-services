0ca0740d25bf90f24d4eaacf40f636ae6cd6d595
Lucas Felipe <conhecimento54321@gmail.com>
2021-04-23 23:05:59 -0300

    Create "useIngredientsChecklistContext" hook to ingredientsChecklist context provider

---
+++ frontend/src/contexts/IngredientsChecklist/hooks/useIngredientsChecklistContext.js
@@ -0,0 +1,16 @@
+import { useContext } from 'react'
+import IngredientsChecklistContext from '../context'
+
+export default function useIngredientsChecklistContext() {
+  const {
+    ingredientsChecklist,
+    handleComplementsChecklist,
+    handleRecipesChecklist
+  } = useContext(IngredientsChecklistContext)
+
+  return {
+    ingredientsChecklist,
+    handleComplementsChecklist,
+    handleRecipesChecklist
+  }
+}
