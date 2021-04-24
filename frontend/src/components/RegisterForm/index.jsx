6cb386333220c117919e85f655e525378d3fee51
Lucas Felipe <conhecimento54321@gmail.com>
2021-04-24 14:34:08 -0300

    Componentization of  old <img> tag in RegisterForm component

--- frontend/src/components/RegisterForm/index.jsx
+++ frontend/src/components/RegisterForm/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 
-import useThumbnail from '../../hooks/thubmbnail/useThumbnail'
 import useEmail from '../../hooks/registerFormHooks/email/useEmail'
 import useFullname from '../../hooks/registerFormHooks/fullName/useFullname'
 
@@ -9,29 +8,17 @@ import FullName from '../../parts/FormParts/FullName'
 import RegisterButton from '../../parts/FormParts/RegisterButton'
 
 import useRegisterFormStyles from './styles'
+import RecipeLogo from '../../parts/GLOBAL/RecipeLogo'
 
 export default function RegisterForm() {
   const { fullName, handleFullName } = useFullname()
   const { email, handleEmail } = useEmail()
 
-  const {
-    thumbnailIngredientDescription,
-    thumbnailIngredientPath,
-    thumbnailIngredientName,
-  } = useThumbnail()
-
-  console.log(thumbnailIngredientDescription)
-
   const styles = useRegisterFormStyles()
 
   return (
     <form className={styles.form} >
-      <img
-        src={`${process.env.PUBLIC_URL}${thumbnailIngredientPath}`}
-        alt={thumbnailIngredientDescription}
-        className={styles.logo}
-        title={thumbnailIngredientName}
-      />
+      <RecipeLogo big />
       <div className={styles.inputsWrapper}>
         <FullName
           fullName={fullName}
