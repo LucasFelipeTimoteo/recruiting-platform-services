3cd524a7fb26a28e954fc81556390e9240ea86a3
Lucas Felipe <conhecimento54321@gmail.com>
2021-04-24 00:02:22 -0300

    Create ActionsButtons part

--- frontend/src/parts/ComplementsSelect/ActionsButtons/index.jsx
+++ frontend/src/parts/ComplementsSelect/ActionsButtons/index.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { Button, ButtonGroup } from '@material-ui/core'
 import useActionsButtonsStyles from './styles'
 
-export default function ActionsButtons() {
+export default function ActionsButtons({setRecipesPageStep}) {
   const styles = useActionsButtonsStyles()
 
   return (
     <ButtonGroup className={styles.buttonGroup}>
       <Button
         className={`${styles.buttonsbaseStyle} ${styles.returnButtonColor}`}
+        onClick={setRecipesPageStep}
       >
         Voltar
     </Button>
