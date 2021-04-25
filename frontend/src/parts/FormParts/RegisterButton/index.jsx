c8e43a793a9540979c64771fa33a36403e2a7dc7
Lucas Felipe <conhecimento54321@gmail.com>
2021-04-25 00:35:34 -0300

    Add handleSumit util to RegisterButton part

--- frontend/src/parts/FormParts/RegisterButton/index.jsx
+++ frontend/src/parts/FormParts/RegisterButton/index.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import { Button } from '@material-ui/core'
 import useRegisterButton from './styles'
+import handleSubmit from '../../../utils/RegisterForm/form/handleSubmit'
 
-export default function RegisterButton() {
+export default function RegisterButton({ fullName, email }) {
   const styles = useRegisterButton()
 
   return (
     <>
       <Button
-        type="button"
+        type="submit"
         color="primary"
         variant="contained"
         size="large"
         disableElevation={true}
         className={styles.button}
+        onClick={(event) => handleSubmit(fullName, email, event)}
       >
         cadastrar
       </Button>
