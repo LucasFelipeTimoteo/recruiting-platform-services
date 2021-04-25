3a4385b2a388e1c3b3a9de0c3e2a1389e9e158f1
Lucas Felipe <conhecimento54321@gmail.com>
2021-04-25 18:45:37 -0300

    Remove getLocalStorageData

--- frontend/src/utils/GLOBAL/getLocalStorageData/index.js
+++
@@ -1,8 +0,0 @@
-const getLocalStorageData = () => {
-  const storage = localStorage.getItem('SMART_COFFEE')
-  const parsedStorage = storage ? JSON.parse(storage) : null
-
-  return parsedStorage
-}
-
-export default getLocalStorageData
