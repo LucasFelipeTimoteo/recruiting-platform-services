8e1ba416064e3f5ea45d5ebfcce325c11bd8075b
Lucas Felipe <conhecimento54321@gmail.com>
2021-04-25 00:10:52 -0300

    Add new route to app.js and configure express.json()

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,9 +2,13 @@ const express = require('express')
 const cors = require('cors')
 
 const recipesRouter = require('./routes/recipesRouter')
+const usersRouter = require('./routes/usersRouter')
 
 const app = express()
 app.use(cors())
+app.use(express.json())
+
 app.use('/ingredients', recipesRouter)
+app.use('/users', usersRouter)
 
 module.exports = app
