6f520665765298c5585d2363fd7665f6b7bf835d
Lucas Felipe <conhecimento54321@gmail.com>
2021-04-26 15:38:04 -0300

    Add useCoffeeSelectStyles styles to CoffeeSelect component

---
+++ frontend/src/components/CoffeSelect/styles.js
@@ -0,0 +1,13 @@
+import { makeStyles } from "@material-ui/core";
+
+const useCoffeeSelectStyles = makeStyles({
+  coffeeSelectWrapper: {
+    width: '100%',
+    height: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  }
+})
+
+export default useCoffeeSelectStyles
