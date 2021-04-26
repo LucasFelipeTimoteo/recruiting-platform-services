39e9dd19b4c7a26d174eefe33553f1874f995236
Lucas Felipe <conhecimento54321@gmail.com>
2021-04-26 13:09:16 -0300

    Update useListItemStyles  removing some styles and fix width

--- frontend/src/parts/RecipesSelectParts/RecipeListItem/styles.js
+++ frontend/src/parts/RecipesSelectParts/RecipeListItem/styles.js
@@ -1,20 +1,15 @@
 import { makeStyles } from "@material-ui/core";
 
-const useListItemStyles = makeStyles(theme => ({
-  listItem: {
+const useListItemStyles = makeStyles({
+  recipeListItem: {
     display: 'flex',
     justifyContent: 'flex-start',
     alignItems: 'center',
 
-    width: '90vw',
-    padding: '7px 0',
-    border: '10px solid transparent',
-    borderBottom: '2px solid lightgray',
-
-    [theme.breakpoints.down(theme.breakpoints.values.sm)]: {
-      width: '96vw'
-    }
-  },
-}))
+    width: '100%',
+    padding: '12px 0',
+    borderBottom: '2px solid lightgray'
+  }
+})
 
 export default useListItemStyles
