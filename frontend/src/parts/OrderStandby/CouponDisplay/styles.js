9004e11bdce800d3d97ea99e081057e90c72b414
Lucas Felipe <conhecimento54321@gmail.com>
2021-04-28 23:37:26 -0300

    Create useCouponDisplayStyles styles

---
+++ frontend/src/parts/OrderStandby/CouponDisplay/styles.js
@@ -0,0 +1,13 @@
+import { makeStyles } from "@material-ui/core";
+
+const useCouponDisplayStyles = makeStyles({
+  couponContainer: {
+    width: '100%',
+    border: '5px dashed #b0b0b0',
+    padding: 10,
+    textAlign: 'center',
+    marginTop: 'auto'
+  }
+})
+
+export default useCouponDisplayStyles
