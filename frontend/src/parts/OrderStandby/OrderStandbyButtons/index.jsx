16129dd7069baab3a892cdc59cfec092e48a3c48
Lucas Felipe <conhecimento54321@gmail.com>
2021-04-28 23:29:07 -0300

    Create OrderStandbyButtons buttons

---
+++ frontend/src/parts/OrderStandby/OrderStandbyButtons/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Button, IconButton } from '@material-ui/core'
+import { RotateLeft } from '@material-ui/icons'
+import handleBackToHome from '../../../utils/OrderStandby/handleBackToHome'
+
+import useOrderStandbyButtonsStyles from './styles'
+import printCoupon from '../../../utils/OrderStandby/printCoupon'
+
+export default function OrderStandbyButtons() {
+  const styles = useOrderStandbyButtonsStyles()
+
+  return (
+    <div className={styles.buttonGroup}>
+      <IconButton
+        className={styles.returnButton}
+        onClick={handleBackToHome}
+      >
+        <RotateLeft />
+      </IconButton>
+
+      <Button
+        color="primary"
+        variant="contained"
+        disableElevation
+        className={styles.printCouponButton}
+        onClick={printCoupon}
+      >
+        imprimir cupom
+      </Button>
+    </div>
+  )
+}
