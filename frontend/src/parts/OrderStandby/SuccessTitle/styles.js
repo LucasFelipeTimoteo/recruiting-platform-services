20bcd533444a3c8595091bcaf87a63d41a967510
Lucas Felipe <conhecimento54321@gmail.com>
2021-04-28 23:31:43 -0300

    Create useSuccessTitleStyles styles

---
+++ frontend/src/parts/OrderStandby/SuccessTitle/styles.js
@@ -0,0 +1,10 @@
+import { makeStyles } from "@material-ui/core";
+
+const useSuccessTitleStyles = makeStyles({
+  successTitle: {
+    textAlign: 'center',
+    margin: '40px 0 30px 0'
+  }
+})
+
+export default useSuccessTitleStyles
