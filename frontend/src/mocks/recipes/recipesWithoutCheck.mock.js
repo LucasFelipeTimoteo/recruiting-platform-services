56e1dc2f3a0c718630e6bbd99c9f479353a67555
Lucas Felipe <conhecimento54321@gmail.com>
2021-05-04 15:43:31 -0300

    Create mock recipesWithoutCheck

---
+++ frontend/src/mocks/recipes/recipesWithoutCheck.mock.js
@@ -0,0 +1,48 @@
+const recipesWithoutCheck = [
+  {
+    id: 1,
+    name: 'recipe1',
+    time: 10,
+    complements: [
+      {
+        id: 1,
+        name: "complement1",
+        time: 15
+      },
+      {
+        id: 2,
+        name: "complement2",
+        time: 20
+      },
+      {
+        id: 3,
+        name: "complement3",
+        time: 25
+      },
+    ]
+  },
+  {
+    id: 2,
+    name: 'recipe2',
+    time: 30,
+    complements: [
+      {
+        id: 1,
+        name: "complement1",
+        time: 15
+      },
+      {
+        id: 2,
+        name: "complement2",
+        time: 20
+      },
+      {
+        id: 4,
+        name: "complement4",
+        time: 40
+      },
+    ]
+  },
+]
+
+export default recipesWithoutCheck
