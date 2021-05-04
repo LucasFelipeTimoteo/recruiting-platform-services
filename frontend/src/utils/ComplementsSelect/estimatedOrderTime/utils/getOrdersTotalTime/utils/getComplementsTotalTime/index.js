2a66fdc7f46d0f9c9aeb675e739fad0390031fb4
Lucas Felipe <conhecimento54321@gmail.com>
2021-05-04 00:03:09 -0300

    Delete getTotalIngredientsTimeSeconds

--- frontend/src/utils/ComplementsSelect/estimatedOrderTime/utils/getOrdersTotalTime/utils/getComplementsTotalTime/index.js
+++
@@ -1,9 +0,0 @@
-import totalIngredientsTimeInMinutes from "../totalIngredientsTimeInMinutes"
-
-const getComplementsTotalTime = (complementsTimes) => {
-  const recipeTimeInSecounds = totalIngredientsTimeInMinutes(complementsTimes)
-
-  return recipeTimeInSecounds
-}
-
-export default getComplementsTotalTime
