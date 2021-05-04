476dbd9d89bd2fb8fc83837ed25159312a280811
Lucas Felipe <conhecimento54321@gmail.com>
2021-05-03 23:58:32 -0300

    Refactoring toggleComplementsChecklist adding isTheSelectedIngredient util function in conditionals

--- frontend/src/utils/ComplementsSelect/toggleComplementsChecklist/index.js
+++ frontend/src/utils/ComplementsSelect/toggleComplementsChecklist/index.js
@@ -1,15 +1,24 @@
+import isTheSelectedIngredient from "./utils/isTheSelectedIngredient"
+
 const toggleComplementsChecklist = (
   recipesChecklist,
   selectedComplement,
   currentRecipe
 ) => {
   const newComplementsChecklist = recipesChecklist.map(recipe => {
-    if(!recipe.checked){
+    if (!recipe.checked) {
       return recipe
-    } 
+    }
 
     recipe.complements.map(complement => {
-      if(selectedComplement.id === complement.id && recipe.id === currentRecipe.id ){
+      const selectedComplementIsEqualComplement = isTheSelectedIngredient(
+        selectedComplement.id, complement.id
+      )
+      const currentRecipeIsEqualRecipe = isTheSelectedIngredient(
+        recipe.id, currentRecipe.id
+      )
+
+      if (selectedComplementIsEqualComplement && currentRecipeIsEqualRecipe) {
         complement.checked = !complement.checked
       }
 
