b6b533c2bd5a0ee894aa4bee316037b6311f6d4d
Lucas Felipe <conhecimento54321@gmail.com>
2021-05-03 23:15:21 -0300

    Create complementsTimes util function

---
+++ frontend/src/utils/ComplementsSelect/estimatedOrderTime/utils/getComplementsTimes/utils/complementsTimes/index.js
@@ -0,0 +1,7 @@
+const complementsTimes = (complementsTimesList) => {
+  const orderComplementsTimes = complementsTimesList.flat()
+
+  return orderComplementsTimes
+}
+
+export default complementsTimes
