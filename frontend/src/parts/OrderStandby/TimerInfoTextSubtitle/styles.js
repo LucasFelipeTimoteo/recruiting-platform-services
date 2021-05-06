69eef3b8f73d441af2caa2ec39bfa7f41aafc971
Lucas Felipe <conhecimento54321@gmail.com>
2021-05-06 09:15:23 -0300

    Create useTimerInfoTextSubtitleStyles styles

---
+++ frontend/src/parts/OrderStandby/TimerInfoTextSubtitle/styles.js
@@ -0,0 +1,9 @@
+import { makeStyles } from "@material-ui/core";
+
+const useTimerInfoTextSubtitleStyles = makeStyles({
+  timerInfoTextSubtitle: {
+    color: '#5E4429'
+  }
+})
+
+export default useTimerInfoTextSubtitleStyles
