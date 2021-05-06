d7a685f97e173f099da5fe36b70f59be09c79c94
Lucas Felipe <conhecimento54321@gmail.com>
2021-05-06 18:54:55 -0300

    Add sync between timer and localStorage - add orderTime from useOrdersTotalTimeContext as timer duration - add handleOrderTime to save current time on storage in each render

--- frontend/src/parts/OrderStandby/Timer/index.jsx
+++ frontend/src/parts/OrderStandby/Timer/index.jsx
@@ -2,22 +2,29 @@ import React from 'react'
 import { CountdownCircleTimer } from 'react-countdown-circle-timer'
 import TimerInfoText from '../TimerInfoText'
 import ringBell from '../../../utils/OrderStandby/ringBell'
+import useOrdersTotalTimeContext from '../../../contexts/ordersTotalTimes/hooks/useOrdersTotalTimeContext'
 
 export default function Timer() {
+  const { orderTime, handleOrderTime } = useOrdersTotalTimeContext()
+
   return (
     <CountdownCircleTimer
       id="countdown"
       size={250}
       isPlaying
-      duration={10}
-      initialRemainingTime={10}
+      duration={orderTime}
+      initialRemainingTime={orderTime}
       colors={[['#2ee897', 0.70], ['#F7B801', 0.28], ['#A30000']]}
       onComplete={ringBell}
     >
       {
-        ({ remainingTime }) => (
-          <TimerInfoText remainingTime={remainingTime} />
-        )
+        ({ remainingTime }) => {
+          handleOrderTime(remainingTime - 1)
+          
+          return (
+            <TimerInfoText remainingTime={remainingTime} />
+          )
+        }
       }
     </CountdownCircleTimer>
   )
