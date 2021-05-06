c3918ed055c0dbb78e43495bd222c644c8015e0e
Lucas Felipe <conhecimento54321@gmail.com>
2021-05-06 09:12:07 -0300

    Create useTimerInfoTextStyles styles

---
+++ frontend/src/parts/OrderStandby/TimerInfoText/styles.js
@@ -0,0 +1,20 @@
+import { makeStyles } from "@material-ui/core";
+
+const useTimerInfoTextStyles = makeStyles({
+  timerInfoTextContainer: {
+    textAlign: 'center',
+    width: 130
+  },
+
+  timerInfoTextTitle: {
+    margin: '30px 0 10px',
+    fontWeight: 700,
+    color: '#5E4429'
+  },
+  
+  timerInfoTextSubtitle: {
+    color: '#5E4429'
+  }
+})
+
+export default useTimerInfoTextStyles
