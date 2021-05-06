6e36f8d180f7f08d9fb9071ff76875c329b2af16
Lucas Felipe <conhecimento54321@gmail.com>
2021-05-06 18:30:54 -0300

    Create timerDisplay util function

---
+++ frontend/src/utils/OrderStandby/timerDisplay/index.js
@@ -0,0 +1,9 @@
+import displayCounterTime from "./utils/displayCounterTime"
+
+const timerDisplay = (remainingTime) => {
+  const counterTime = displayCounterTime(remainingTime)
+
+  return counterTime
+}
+
+export default timerDisplay
