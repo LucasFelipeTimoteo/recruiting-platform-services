55256fc7f8d503cbbcb4155f408ba7e702c0e3b6
Lucas Felipe <conhecimento54321@gmail.com>
2021-05-06 09:09:34 -0300

    Add Timer part to OrderStandby component

--- frontend/src/components/OrderStandby/index.jsx
+++ frontend/src/components/OrderStandby/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+
 import CouponDisplay from '../../parts/OrderStandby/CouponDisplay'
 import OrderStandbyButtons from '../../parts/OrderStandby/OrderStandbyButtons'
 import SuccessTitle from '../../parts/OrderStandby/SuccessTitle'
+import Timer from '../../parts/OrderStandby/Timer'
 import useOrderStandbyStyles from './styles'
 
 export default function OrderStandby() {
@@ -10,11 +12,7 @@ export default function OrderStandby() {
   return (
     <div className={styles.orderStandbyWrapper}>
       <SuccessTitle />
-      
-      <div style={{ height: 250, width: '100%', background: 'gray' }}>
-        TIMER
-      </div>
-      
+      <Timer />
       <CouponDisplay />
       <OrderStandbyButtons />
     </div>
