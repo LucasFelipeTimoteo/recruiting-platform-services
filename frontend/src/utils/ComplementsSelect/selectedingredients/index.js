bdfa461e947484f04eee020581595937cc14f1f4
Lucas Felipe <conhecimento54321@gmail.com>
2021-05-06 09:42:59 -0300

    Rename selectedingredients util function to getSelectedIngredients and selectedIngredients var to selectedIngredientsList

--- frontend/src/utils/ComplementsSelect/selectedingredients/index.js
+++ frontend/src/utils/ComplementsSelect/selectedingredients/index.js
@@ -1,12 +1,12 @@
-const selectedingredients = (selectedRecipesList) => {
-  const selectedIngredients = selectedRecipesList.map(recipe => {
+const getSelectedIngredients = (selectedRecipesList) => {
+  const selectedIngredientsList = selectedRecipesList.map(recipe => {
     const selectedComplements = recipe.complements.filter(complement => complement.checked)
     const selectedIngredients = {...recipe, complements: selectedComplements}
 
     return selectedIngredients
   })
 
-  return selectedIngredients
+  return selectedIngredientsList
 }
 
-export default selectedingredients
+export default getSelectedIngredients
