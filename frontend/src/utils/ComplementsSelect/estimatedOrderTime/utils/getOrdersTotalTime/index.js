3bf5f776dc7096900a80615109f9ea00ec69e7ec
Lucas Felipe <conhecimento54321@gmail.com>
2021-05-06 18:20:15 -0300

    Refactoring - Remove isMinuteInteger and parsedIngredientsTotalTime and add ingredientsTotalTimeInSeconds

--- frontend/src/utils/ComplementsSelect/estimatedOrderTime/utils/getOrdersTotalTime/index.js
+++ frontend/src/utils/ComplementsSelect/estimatedOrderTime/utils/getOrdersTotalTime/index.js
@@ -1,19 +1,30 @@
 import getComplementsTotalTimeInSeconds from "./utils/getComplementsTotalTimeInSeconds"
 import getRecipesTotalTimeInSeconds from "./utils/getRecipesTotalTimeInSeconds"
 import getIngredientsTotalTime from "./utils/getIngredientsTotalTime"
-import parseNumberToMinutes from "../../../../GLOBAL/parseNumberToMinutes"
+import getIngredientsTotalTimeInSeconds from "./utils/getIngredientsTotalTimeInSeconds"
 
 const getOrdersTotalTime = (recipesTimes, complementsTimes) => {
   const recipesTotalTimeInSeconds = getRecipesTotalTimeInSeconds(recipesTimes)
-  const complementsTotalTimeInSeconds = getComplementsTotalTimeInSeconds(complementsTimes)
-  
-  const allIngredientsTimesSecondsList = [recipesTotalTimeInSeconds, complementsTotalTimeInSeconds]
-  const ingredientsTotalTime = getIngredientsTotalTime(allIngredientsTimesSecondsList)
+  const complementsTotalTimeInSeconds = getComplementsTotalTimeInSeconds(
+    complementsTimes
+  )
 
-  const isMinuteInteger = Number.isInteger(ingredientsTotalTime)
-  const parsedIngredientsTotalTime = parseNumberToMinutes(ingredientsTotalTime, isMinuteInteger)
+  const allIngredientsTimesSecondsList = [
+    recipesTotalTimeInSeconds,
+    complementsTotalTimeInSeconds
+  ]
 
-  return parsedIngredientsTotalTime
+  const ingredientsTotalTime = getIngredientsTotalTime(
+    allIngredientsTimesSecondsList
+  )
+  const ingredientsTotalTimeInSeconds = getIngredientsTotalTimeInSeconds(
+    allIngredientsTimesSecondsList
+  )
+
+  return {
+    ingredientsTotalTime,
+    ingredientsTotalTimeInSeconds
+  }
 }
 
 export default getOrdersTotalTime
