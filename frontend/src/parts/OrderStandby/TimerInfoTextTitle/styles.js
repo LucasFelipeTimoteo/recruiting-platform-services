af14c9be8d91c2fcfecb9eb76e836b5aab0559e6
Lucas Felipe <conhecimento54321@gmail.com>
2021-05-06 18:35:42 -0300

    Refactoring - update useTimerInfoTextTitleStyles styles: margin and fontWeight

--- frontend/src/parts/OrderStandby/TimerInfoTextTitle/styles.js
+++ frontend/src/parts/OrderStandby/TimerInfoTextTitle/styles.js
@@ -2,8 +2,8 @@ import { makeStyles } from "@material-ui/core";
 
 const useTimerInfoTextTitleStyles = makeStyles({
   timerInfoTextTitle: {
-    margin: '30px 0 10px',
-    fontWeight: 700,
+    margin: '30px 0 10px 0',
+    fontWeight: 600,
     color: '#5E4429',
     letterSpacing: 0
   }
