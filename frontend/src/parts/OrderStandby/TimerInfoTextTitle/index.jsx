66affa0a3efa0f32cd1b085296a97153ab80182a
Lucas Felipe <conhecimento54321@gmail.com>
2021-05-06 23:38:21 -0300

    Add handleOrderTime inside useEffect to avoid render exceptions

--- frontend/src/parts/OrderStandby/TimerInfoTextTitle/index.jsx
+++ frontend/src/parts/OrderStandby/TimerInfoTextTitle/index.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Typography } from '@material-ui/core'
 import useTimerInfoTextTitleStyles from './styles'
 import timerDisplay from '../../../utils/OrderStandby/timerDisplay'
 
-export default function TimerInfoTextTitle({ remainingTime }) {
+export default function TimerInfoTextTitle({ remainingTime, handleOrderTime }) {
   const timerDisplayCounter = timerDisplay(remainingTime)
+  useEffect(() => {
+    const storeCondition = remainingTime > 0 ? remainingTime - 1 : remainingTime
+    handleOrderTime(storeCondition)
+  }, [remainingTime, handleOrderTime])
+
   const styles = useTimerInfoTextTitleStyles()
 
   return (
