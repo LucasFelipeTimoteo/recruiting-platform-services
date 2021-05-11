9dc30db70c53f788f80c45a040b44ec0cfee4850
Lucas Felipe <conhecimento54321@gmail.com>
2021-05-10 23:58:21 -0300

    Add conditional styling to useComplementsListStyles
    to fix an UI bug, set "margin: '12px 0'" when page is below 320px

--- frontend/src/parts/ComplementsSelect/ComplementsList/styles.js
+++ frontend/src/parts/ComplementsSelect/ComplementsList/styles.js
@@ -1,9 +1,13 @@
 import { makeStyles } from "@material-ui/core";
 
-const useComplementsListStyles = makeStyles({
+const useComplementsListStyles = makeStyles(theme => ({
   complementsListWrapper: {
     padding: 0,
-    margin: '12px 25px'
+    margin: '12px 25px',
+
+    [theme.breakpoints.down(320)]: {
+      margin: '12px 0',
+    }
   },
 
   complementInfoGroup: {
@@ -11,6 +15,6 @@ const useComplementsListStyles = makeStyles({
     flexDirection: 'column',
     alignItems: 'flex-start',
   }
-})
+}))
 
 export default useComplementsListStyles
