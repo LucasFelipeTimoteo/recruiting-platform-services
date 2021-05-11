b122b9f8564768253fa0a5fbb81d736aefb13404
Lucas Felipe <conhecimento54321@gmail.com>
2021-05-11 17:42:19 -0300

    Fix returned data format: should return string data - old: number - new: string

--- frontend/src/utils/OrderStandby/timerDisplay/utils/displayCounterTime/utils/displayMinutes/index.js
+++ frontend/src/utils/OrderStandby/timerDisplay/utils/displayCounterTime/utils/displayMinutes/index.js
@@ -1,7 +1,8 @@
 const displayMinutes = (remainingTime) => {
   const minutes = Math.floor(remainingTime / 60)
+  const stringMinutes = String(minutes)
 
-  return minutes
+  return stringMinutes
 }
 
 export default displayMinutes
