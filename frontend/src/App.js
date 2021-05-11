9c3471bab36d29e0752079fd05cab3d0430a457f
Lucas Felipe <conhecimento54321@gmail.com>
2021-05-10 23:46:14 -0300

    Put UserRegistryProvider as app wrapper in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,19 @@ import { CssBaseline, ThemeProvider } from '@material-ui/core';
 import MuiTheme from './themes/MuiThemes/MuiTheme';
 import './App.css';
 import OrdersTotalTimeProvider from './contexts/ordersTotalTimes/provider';
+import UserRegistryProvider from './contexts/userRegister/provider';
 
 function App() {
   return (
     <ThemeProvider theme={MuiTheme}>
-      <OrdersTotalTimeProvider>
-        <div className="App">
-          <CssBaseline />
-          <Routes />
-        </div>
-      </OrdersTotalTimeProvider>
+      <UserRegistryProvider>
+        <OrdersTotalTimeProvider>
+          <div className="App">
+            <CssBaseline />
+            <Routes />
+          </div>
+        </OrdersTotalTimeProvider>
+      </UserRegistryProvider>
     </ThemeProvider>
   );
 }
