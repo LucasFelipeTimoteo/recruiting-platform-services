a7af4a964a61835b16bb410ac692ac0f9efadda7
Lucas Felipe <conhecimento54321@gmail.com>
2021-05-11 00:46:21 -0300

    Import handleUser from useUserRegisterContext and pass as prop to handleSubmit

--- frontend/src/parts/FormParts/RegisterButton/index.jsx
+++ frontend/src/parts/FormParts/RegisterButton/index.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import { Button } from '@material-ui/core'
 import useRegisterButton from './styles'
 import handleSubmit from '../../../utils/RegisterForm/form/handleSubmit'
+import useUserRegisterContext from '../../../contexts/userRegister/hooks/useUserRegisterContext'
 
 export default function RegisterButton({ fullName, email }) {
+  const { handleUser } = useUserRegisterContext()
   const styles = useRegisterButton()
 
   return (
@@ -15,7 +17,12 @@ export default function RegisterButton({ fullName, email }) {
         size="large"
         disableElevation={true}
         className={styles.registerButton}
-        onClick={(event) => handleSubmit(fullName, email, event)}
+        onClick={(event) => handleSubmit({
+          handleUser,
+          fullName,
+          email,
+          event
+        })}
       >
         cadastrar
       </Button>
