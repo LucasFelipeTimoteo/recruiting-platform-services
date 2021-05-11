a5be04fd72d8eb0188dc44a067fc2f1d16600bac
Lucas Felipe <conhecimento54321@gmail.com>
2021-05-10 23:48:26 -0300

    Add ComplementsSelectWrapper styles to useComplementsSelectStyles

--- frontend/src/components/ComplementsSelect/styles.js
+++ frontend/src/components/ComplementsSelect/styles.js
@@ -1,6 +1,14 @@
 import { makeStyles } from "@material-ui/core";
 
 const useComplementsSelectStyles = makeStyles({
+  ComplementsSelectWrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '100%',
+    maxWidth: 800,
+    height: '100%',
+  },
   listTitle: {
     margin: 17
   }
