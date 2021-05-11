949f9abfb69cb6cf1a16dfbf7e924f9d8670d580
Lucas Felipe <conhecimento54321@gmail.com>
2021-05-11 17:43:13 -0300

    Replace conterTime logic to displayParsedTotalTime util function in displayCounterTime

--- frontend/src/utils/OrderStandby/timerDisplay/utils/displayCounterTime/index.js
+++ frontend/src/utils/OrderStandby/timerDisplay/utils/displayCounterTime/index.js
@@ -1,11 +1,11 @@
 import displayMinutes from './utils/displayMinutes'
+import displayParsedTotalTime from './utils/displayParsedTotalTime'
 import displaySeconds from './utils/displaySeconds'
 
 const displayCounterTime = (remainingTime) => {
   const minutes = displayMinutes(remainingTime)
   const seconds = displaySeconds(remainingTime)
-
-  const counterTime = `${minutes}:${seconds} min`
+  const counterTime = displayParsedTotalTime(seconds, minutes)
 
   return counterTime
 }
