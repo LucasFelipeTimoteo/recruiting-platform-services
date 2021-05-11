ceed0b66f0d1fd7d58802ba7fa6914d0af9aec07
Lucas Felipe <conhecimento54321@gmail.com>
2021-05-11 00:10:09 -0300

    Add post requisition to backend  and handleUser state setter in handleSubmit

--- frontend/src/utils/RegisterForm/form/handleSubmit/index.js
+++ frontend/src/utils/RegisterForm/form/handleSubmit/index.js
@@ -1,12 +1,21 @@
 import api from "../../../../services/api"
 
-const handleSubmit = async (fullName, email, event ) => {
+const handleSubmit = async ({
+  handleUser,
+  fullName,
+  email,
+  event
+}) => {
   event.preventDefault()
+  
   try {
     const requestData = { fullName, email }
     const response = await api.post('/users', requestData)
-    console.log(response.data)
-    window.location="/coffees"
+    const savedUserData = response.data
+
+    handleUser(savedUserData)
+
+    window.location = "/coffees"
   }
   catch (error) {
     console.log(error)
