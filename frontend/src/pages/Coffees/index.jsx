75caae7166b139e6e0f7f767c04887f4c7cc2e6a
Lucas Felipe <conhecimento54321@gmail.com>
2021-05-10 23:53:20 -0300

    OPTIMIZATION - Implements lazy load and suspense in Coffees page - lazy loaded: CoffeeSelect and IngredientsChecklistProvider

--- frontend/src/pages/Coffees/index.jsx
+++ frontend/src/pages/Coffees/index.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
-import IngredientsChecklistProvider from '../../contexts/IngredientsChecklist/provider'
-import CoffeeSelect from '../../components/CoffeSelect'
+import React, { lazy } from 'react'
 import Header from '../../components/Header'
+import { Suspense } from 'react'
+
+const CoffeeSelect = lazy(() => import('../../components/CoffeSelect'))
+const IngredientsChecklistProvider = lazy(() => (
+  import('../../contexts/IngredientsChecklist/provider')
+))
 
 export default function Coffees() {
   return (
     <>
       <Header />
-      <IngredientsChecklistProvider>
-        <CoffeeSelect />
-      </IngredientsChecklistProvider>
+      <Suspense fallback={<h1>LOADING...</h1>}>
+        <IngredientsChecklistProvider>
+          <CoffeeSelect />
+        </IngredientsChecklistProvider>
+      </Suspense>
     </>
   )
 }
