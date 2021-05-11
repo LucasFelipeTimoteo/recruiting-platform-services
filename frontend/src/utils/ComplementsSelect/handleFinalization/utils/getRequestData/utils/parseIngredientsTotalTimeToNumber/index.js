df4db1f87eba49426c0cc1f0748e78f70861a71a
Lucas Felipe <conhecimento54321@gmail.com>
2021-05-11 00:22:31 -0300

    Create parseIngredientsTotalTimeToNumber util function

---
+++ frontend/src/utils/ComplementsSelect/handleFinalization/utils/getRequestData/utils/parseIngredientsTotalTimeToNumber/index.js
@@ -0,0 +1,7 @@
+const parseIngredientsTotalTimeToNumber = (ingredientsTotalTime) => {
+  const parsedIngredientsTotalTime = Number(ingredientsTotalTime)
+
+  return parsedIngredientsTotalTime
+}
+
+export default parseIngredientsTotalTimeToNumber
